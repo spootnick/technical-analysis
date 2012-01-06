cf51b5771e0ce96e32b731f84ef48f32fb3481aa
mlistwan@gmail.com <mlistwan@gmail.com@3856f1eb-9fc3-8537-e037-92a41e21bd5f>
2012-01-06 11:59:24 +0000

    więcej sprawdzania w teście

--- trunk/src/test/java/spootnick/TestRule.java
+++ trunk/src/test/java/spootnick/TestRule.java
@@ -2,6 +2,7 @@ package spootnick;
 
 import static org.testng.Assert.*;
 
+import spootnick.data.QuoteSeries;
 import spootnick.result.Action.Side;
 import spootnick.result.Result;
 import spootnick.runtime.Simulation;
@@ -9,15 +10,20 @@ import spootnick.runtime.TradingRule;
 
 public class TestRule implements TradingRule {
 
+	private Move move;
+	
 	@Override
 	public Move start(Simulation simulation) {
 		assertEquals(simulation.getIndex(), simulation.getWindowSize()-1);
-		return new Move(Side.LONG);
+		QuoteSeries series = simulation.getQuoteSeries();
+		double avg = (series.getClose()[0]+series.getClose()[1])/2;
+		move = new Move(avg,avg);
+		return move;
 	}
 
 	@Override
 	public Move next(Simulation simulation) throws InterruptedException {
-		return new Move();
+		return move;
 	}
 
 	@Override
